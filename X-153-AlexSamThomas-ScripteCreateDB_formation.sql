f6d644211d6fb6ebb85656987767cc65c82a8c15
Samuel Develey <pr23yde@eduvaud.ch>
2022-11-04 08:06:15 +0100

    ajout des scripts

---
+++ X-153-AlexSamThomas-ScripteCreateDB_formation.sql
@@ -0,0 +1,412 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db:3306
+-- Généré le : jeu. 03 nov. 2022 à 20:49
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `db_formation`
+--
+
+DROP DATABASE IF EXISTS db_formation;
+
+create database db_formation;
+use db_formation;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_class`
+--
+
+CREATE TABLE `t_class` (
+  `idClass` binary(16) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `fkPerson` binary(16) NOT NULL,
+  `fkFormation` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_formationPlan`
+--
+
+CREATE TABLE `t_formationPlan` (
+  `idFormation` binary(16) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `desription` varchar(400) NOT NULL,
+  `startYear` date NOT NULL,
+  `endYear` date NOT NULL,
+  `fkPerson` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_formationPlanLesson`
+--
+
+CREATE TABLE `t_formationPlanLesson` (
+  `fkFormation` binary(16) NOT NULL,
+  `fkLesson` binary(16) NOT NULL,
+  `beginDate` date NOT NULL,
+  `endDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_gender`
+--
+
+CREATE TABLE `t_gender` (
+  `idGender` binary(16) NOT NULL,
+  `type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_grade`
+--
+
+CREATE TABLE `t_grade` (
+  `idGrade` binary(16) NOT NULL,
+  `weighting` float NOT NULL,
+  `grade` float NOT NULL,
+  `fkPerson` binary(16) NOT NULL,
+  `fkLesson` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_lesson`
+--
+
+CREATE TABLE `t_lesson` (
+  `idLesson` binary(16) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `fkType` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_person`
+--
+
+CREATE TABLE `t_person` (
+  `idPerson` binary(16) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `birthday` date NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `street` varchar(150) NOT NULL,
+  `streetNumber` varchar(3) NOT NULL,
+  `zip` varchar(4) NOT NULL,
+  `locality` varchar(150) NOT NULL,
+  `fkGender` binary(16) NOT NULL,
+  `insertDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_personRole`
+--
+
+CREATE TABLE `t_personRole` (
+  `fkPerson` binary(16) NOT NULL,
+  `fkRole` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_role`
+--
+
+CREATE TABLE `t_role` (
+  `idRole` binary(16) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_section`
+--
+
+CREATE TABLE `t_section` (
+  `idSection` binary(16) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `acronym` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_student`
+--
+
+CREATE TABLE `t_student` (
+  `fkPerson` binary(16) NOT NULL,
+  `fkSection` binary(16) NOT NULL,
+  `fkClass` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_teacher`
+--
+
+CREATE TABLE `t_teacher` (
+  `fkPerson` binary(16) NOT NULL,
+  `acronym` varchar(5) NOT NULL,
+  `fkSection` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_TPI`
+--
+
+CREATE TABLE `t_TPI` (
+  `idTPI` binary(16) NOT NULL,
+  `grade` float NOT NULL,
+  `fkPerson` binary(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_type`
+--
+
+CREATE TABLE `t_type` (
+  `idType` binary(16) NOT NULL,
+  `type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `t_class`
+--
+ALTER TABLE `t_class`
+  ADD PRIMARY KEY (`idClass`),
+  ADD UNIQUE KEY `ID_t_class_IND` (`idClass`),
+  ADD KEY `FKmanage_IND` (`fkPerson`),
+  ADD KEY `FKhave_IND` (`fkFormation`);
+
+--
+-- Index pour la table `t_formationPlan`
+--
+ALTER TABLE `t_formationPlan`
+  ADD PRIMARY KEY (`idFormation`),
+  ADD UNIQUE KEY `ID_t_formationPlans_IND` (`idFormation`),
+  ADD KEY `FKattribute_IND` (`fkPerson`);
+
+--
+-- Index pour la table `t_formationPlanLesson`
+--
+ALTER TABLE `t_formationPlanLesson`
+  ADD PRIMARY KEY (`fkLesson`,`fkFormation`),
+  ADD UNIQUE KEY `ID_contain_IND` (`fkLesson`,`fkFormation`),
+  ADD KEY `FKcon_t_f_IND` (`fkFormation`);
+
+--
+-- Index pour la table `t_gender`
+--
+ALTER TABLE `t_gender`
+  ADD PRIMARY KEY (`idGender`),
+  ADD UNIQUE KEY `ID_t_gender_IND` (`idGender`);
+
+--
+-- Index pour la table `t_grade`
+--
+ALTER TABLE `t_grade`
+  ADD PRIMARY KEY (`idGrade`),
+  ADD UNIQUE KEY `ID_t_grade_IND` (`idGrade`),
+  ADD KEY `FKgrant_IND` (`fkPerson`),
+  ADD KEY `FKassign_IND` (`fkLesson`);
+
+--
+-- Index pour la table `t_lesson`
+--
+ALTER TABLE `t_lesson`
+  ADD PRIMARY KEY (`idLesson`),
+  ADD UNIQUE KEY `ID_t_lesson_IND` (`idLesson`),
+  ADD KEY `FKtype_IND` (`fkType`);
+
+--
+-- Index pour la table `t_person`
+--
+ALTER TABLE `t_person`
+  ADD PRIMARY KEY (`idPerson`),
+  ADD UNIQUE KEY `ID_t_person_IND` (`idPerson`),
+  ADD KEY `FKisGendered_IND` (`fkGender`);
+
+--
+-- Index pour la table `t_personRole`
+--
+ALTER TABLE `t_personRole`
+  ADD PRIMARY KEY (`fkRole`,`fkPerson`),
+  ADD UNIQUE KEY `ID_own_IND` (`fkRole`,`fkPerson`),
+  ADD KEY `FKown_t_p_IND` (`fkPerson`);
+
+--
+-- Index pour la table `t_role`
+--
+ALTER TABLE `t_role`
+  ADD PRIMARY KEY (`idRole`),
+  ADD UNIQUE KEY `ID_t_role_IND` (`idRole`);
+
+--
+-- Index pour la table `t_section`
+--
+ALTER TABLE `t_section`
+  ADD PRIMARY KEY (`idSection`),
+  ADD UNIQUE KEY `ID_t_section_IND` (`idSection`);
+
+--
+-- Index pour la table `t_student`
+--
+ALTER TABLE `t_student`
+  ADD PRIMARY KEY (`fkPerson`),
+  ADD UNIQUE KEY `FKt_p_t_s_IND` (`fkPerson`),
+  ADD KEY `FKpartOf_IND` (`fkSection`),
+  ADD KEY `FKisIn_IND` (`fkClass`);
+
+--
+-- Index pour la table `t_teacher`
+--
+ALTER TABLE `t_teacher`
+  ADD PRIMARY KEY (`fkPerson`),
+  ADD UNIQUE KEY `FKt_p_t_t_IND` (`fkPerson`),
+  ADD KEY `FKbelong_IND` (`fkSection`);
+
+--
+-- Index pour la table `t_TPI`
+--
+ALTER TABLE `t_TPI`
+  ADD PRIMARY KEY (`idTPI`),
+  ADD UNIQUE KEY `ID_t_TPI_IND` (`idTPI`),
+  ADD KEY `FKpass_IND` (`fkPerson`);
+
+--
+-- Index pour la table `t_type`
+--
+ALTER TABLE `t_type`
+  ADD PRIMARY KEY (`idType`),
+  ADD UNIQUE KEY `ID_t_type_IND` (`idType`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `t_class`
+--
+ALTER TABLE `t_class`
+  ADD CONSTRAINT `FKhave_FK` FOREIGN KEY (`fkFormation`) REFERENCES `t_formationPlan` (`idFormation`),
+  ADD CONSTRAINT `FKmanage_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_teacher` (`fkPerson`);
+
+--
+-- Contraintes pour la table `t_formationPlan`
+--
+ALTER TABLE `t_formationPlan`
+  ADD CONSTRAINT `FKattribute_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_teacher` (`fkPerson`);
+
+--
+-- Contraintes pour la table `t_formationPlanLesson`
+--
+ALTER TABLE `t_formationPlanLesson`
+  ADD CONSTRAINT `FKcon_t_f_FK` FOREIGN KEY (`fkFormation`) REFERENCES `t_formationPlan` (`idFormation`),
+  ADD CONSTRAINT `FKcon_t_l` FOREIGN KEY (`fkLesson`) REFERENCES `t_lesson` (`idLesson`);
+
+--
+-- Contraintes pour la table `t_grade`
+--
+ALTER TABLE `t_grade`
+  ADD CONSTRAINT `FKassign_FK` FOREIGN KEY (`fkLesson`) REFERENCES `t_lesson` (`idLesson`),
+  ADD CONSTRAINT `FKgrant_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_student` (`fkPerson`);
+
+--
+-- Contraintes pour la table `t_lesson`
+--
+ALTER TABLE `t_lesson`
+  ADD CONSTRAINT `FKtype_FK` FOREIGN KEY (`fkType`) REFERENCES `t_type` (`idType`);
+
+--
+-- Contraintes pour la table `t_person`
+--
+ALTER TABLE `t_person`
+  ADD CONSTRAINT `FKisGendered_FK` FOREIGN KEY (`fkGender`) REFERENCES `t_gender` (`idGender`);
+
+--
+-- Contraintes pour la table `t_personRole`
+--
+ALTER TABLE `t_personRole`
+  ADD CONSTRAINT `FKown_t_p_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_person` (`idPerson`),
+  ADD CONSTRAINT `FKown_t_r` FOREIGN KEY (`fkRole`) REFERENCES `t_role` (`idRole`);
+
+--
+-- Contraintes pour la table `t_student`
+--
+ALTER TABLE `t_student`
+  ADD CONSTRAINT `FKisIn_FK` FOREIGN KEY (`fkClass`) REFERENCES `t_class` (`idClass`),
+  ADD CONSTRAINT `FKpartOf_FK` FOREIGN KEY (`fkSection`) REFERENCES `t_section` (`idSection`),
+  ADD CONSTRAINT `FKt_p_t_s_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_person` (`idPerson`);
+
+--
+-- Contraintes pour la table `t_teacher`
+--
+ALTER TABLE `t_teacher`
+  ADD CONSTRAINT `FKbelong_FK` FOREIGN KEY (`fkSection`) REFERENCES `t_section` (`idSection`),
+  ADD CONSTRAINT `FKt_p_t_t_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_person` (`idPerson`);
+
+--
+-- Contraintes pour la table `t_TPI`
+--
+ALTER TABLE `t_TPI`
+  ADD CONSTRAINT `FKpass_FK` FOREIGN KEY (`fkPerson`) REFERENCES `t_student` (`fkPerson`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ X-153-AlexSamThomas-ScripteInsertData.ddl
@@ -0,0 +1,182 @@
+use db_formation;
+
+
+--
+-- Déchargement des données de la table `t_role`
+--
+
+INSERT INTO `t_role` (`idRole`, `role`) VALUES
+(0xb1fe08a05bb211ed937a0242ac120002, 'Doyen'),
+(0xb1fe0c4b5bb211ed937a0242ac120002, 'Directeur'),
+(0xb1fe0e4a5bb211ed937a0242ac120002, 'Directeur-Adjoint'),
+(0xb1fe0ef65bb211ed937a0242ac120002, 'MP'),
+(0xb1fe0f3b5bb211ed937a0242ac120002, 'secrétaire');
+
+--
+-- Déchargement des données de la table `t_section`
+--
+
+INSERT INTO `t_section` (`idSection`, `name`, `acronym`) VALUES
+(0xb20077c45bb211ed937a0242ac120002, 'Informatique', 'Inf'),
+(0xb2007bb05bb211ed937a0242ac120002, 'Bois', 'Bois'),
+(0xb2007dc55bb211ed937a0242ac120002, 'Mecanique', 'Mec'),
+(0xb2007e7f5bb211ed937a0242ac120002, 'Electronique', 'Elec'),
+(0xb2007ec35bb211ed937a0242ac120002, 'Polymecanique', 'Poly');
+
+
+--
+-- Déchargement des données de la table `t_type`
+--
+
+INSERT INTO `t_type` (`idType`, `type`) VALUES
+(0xb20558de5bb211ed937a0242ac120002, 'CIE'),
+(0xb2055c595bb211ed937a0242ac120002, 'module école'),
+(0xb2055e545bb211ed937a0242ac120002, 'pratique'),
+(0xb2055f015bb211ed937a0242ac120002, 'CBE'),
+(0xb2055f425bb211ed937a0242ac120002, 'ECG'),
+(0xb2055f7e5bb211ed937a0242ac120002, 'MPT');
+
+--
+-- Déchargement des données de la table `t_gender`
+--
+
+INSERT INTO `t_gender` (`idGender`, `type`) VALUES
+(0xb202a81e5bb211ed937a0242ac120002, 'Homme'),
+(0xb202ab905bb211ed937a0242ac120002, 'Femme'),
+(0xb202ad795bb211ed937a0242ac120002, 'Autre');
+
+--
+-- Déchargement des données de la table `t_person`
+--
+
+INSERT INTO `t_person` (`idPerson`, `lastname`, `firstname`, `birthday`, `phoneNumber`, `email`, `street`, `streetNumber`, `zip`, `locality`, `fkGender`, `insertDate`) VALUES
+(0xb20818d15bb211ed937a0242ac120002, 'Mich', 'Jean', '2022-11-18', '987/345/34/23', 'jean@gmail.com', 'Ch.de la Chapelle', '4', '2345', 'Echallens', 0xb202a81e5bb211ed937a0242ac120002, '2022-08-27'),
+(0xb2081fcf5bb211ed937a0242ac120002, 'Duck', 'Riri', '2022-07-19', '234/343/23/34', 'Riri@gmail.com', 'Ch. du Rosier', '7', '1234', 'Picsouland', 0xb202a81e5bb211ed937a0242ac120002, '2022-10-13'),
+(0xb20823425bb211ed937a0242ac120002, 'Duck', 'Fifi', '2022-05-17', '234/344/34/23', 'Fifi@gmail.com', 'Ch.de la Mar', '3', '1234', 'Picsouland', 0xb202a81e5bb211ed937a0242ac120002, '2022-11-01'),
+(0xb20825bd5bb211ed937a0242ac120002, 'Duck', 'Loulou', '2022-08-17', '234/343/34/34', 'Loulou@gmail.com', 'Ch. de l\'Etang ', '5', '1243', 'Picsouland', 0xb202a81e5bb211ed937a0242ac120002, '2022-11-01'),
+(0xb20826f35bb211ed937a0242ac120002, 'Grey', 'Julie', '2022-08-15', '234/343/34/32', 'Julie@gmail.com', 'Ch.de la Ropa', '5', '5678', 'Lausanne', 0xb202a81e5bb211ed937a0242ac120002, '2022-10-31'),
+(0xb20828165bb211ed937a0242ac120002, 'Etchebest', 'Philipe', '2022-06-14', '234/344/73/24', 'philipe@gmail.com', 'ch. de la rosie', '4', '5677', 'Paris', 0xb202a81e5bb211ed937a0242ac120002, '2022-11-01'),
+(0xb20829385bb211ed937a0242ac120002, 'Jey', 'Katy', '2022-06-21', '345/343/34/34', 'katy@gmail.com', 'ch.rosey', '4', '2345', 'Lausanne', 0xb202ab905bb211ed937a0242ac120002, '2022-11-01'),
+(0xb2082a4c5bb211ed937a0242ac120002, 'Blandy', 'Josephine', '2022-06-14', '234/343/23/43', 'josephine@gmail.com', 'ch.du Calley', '4', '3456', 'Montreux', 0xb202ab905bb211ed937a0242ac120002, '2022-11-01'),
+(0xb2082b5f5bb211ed937a0242ac120002, 'Sling', 'Brandy', '2022-07-19', '234/342/44/55', 'brandy@gmail.com', 'ch.de la Lepraz', '2', '2345', 'Vevey', 0xb202ab905bb211ed937a0242ac120002, '2022-11-01'),
+(0xb2082c745bb211ed937a0242ac120002, 'Marsu', 'Tomy', '2022-04-05', '234/343/34/34', 'tom@gmail.com', 'ch.de la Roue', '7', '5677', 'Yverdon', 0xb202ad795bb211ed937a0242ac120002, '2022-11-01');
+
+--
+-- Déchargement des données de la table `t_personRole`
+--
+
+INSERT INTO `t_personRole` (`fkPerson`, `fkRole`) VALUES
+(0xb20818d15bb211ed937a0242ac120002, 0xb1fe0c4b5bb211ed937a0242ac120002),
+(0xb2081fcf5bb211ed937a0242ac120002, 0xb1fe08a05bb211ed937a0242ac120002),
+(0xb20823425bb211ed937a0242ac120002, 0xb1fe0e4a5bb211ed937a0242ac120002),
+(0xb20825bd5bb211ed937a0242ac120002, 0xb1fe0ef65bb211ed937a0242ac120002),
+(0xb20826f35bb211ed937a0242ac120002, 0xb1fe0f3b5bb211ed937a0242ac120002);
+
+--
+-- Déchargement des données de la table `t_teacher`
+--
+
+INSERT INTO `t_teacher` (`fkPerson`, `acronym`, `fkSection`) VALUES
+(0xb20828165bb211ed937a0242ac120002, 'pee', 0xb20077c45bb211ed937a0242ac120002),
+(0xb20829385bb211ed937a0242ac120002, 'kjy', 0xb2007bb05bb211ed937a0242ac120002),
+(0xb2082a4c5bb211ed937a0242ac120002, 'jby', 0xb2007dc55bb211ed937a0242ac120002),
+(0xb2082b5f5bb211ed937a0242ac120002, 'bsg', 0xb2007e7f5bb211ed937a0242ac120002),
+(0xb2082c745bb211ed937a0242ac120002, 'tmu', 0xb2007ec35bb211ed937a0242ac120002);
+
+
+--
+-- Déchargement des données de la table `t_formationPlan`
+--
+
+INSERT INTO `t_formationPlan` (`idFormation`, `name`, `desription`, `startYear`, `endYear`, `fkPerson`) VALUES
+(0xed6100165bb511ed937a0242ac120002, 'ICT', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2018-11-10', '2022-11-10', 0xb2082c745bb211ed937a0242ac120002),
+(0xed6106065bb511ed937a0242ac120002, 'ICT avec maturité', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2019-11-10', '2022-11-10', 0xb2082b5f5bb211ed937a0242ac120002),
+(0xed61070e5bb511ed937a0242ac120002, 'Plan 3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2020-11-10', '2022-11-10', 0xb2082a4c5bb211ed937a0242ac120002),
+(0xed6107855bb511ed937a0242ac120002, 'Plan 4', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2017-11-10', '2022-11-10', 0xb20828165bb211ed937a0242ac120002),
+(0xed6107f05bb511ed937a0242ac120002, 'Plan 5', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2019-11-10', '2022-11-10', 0xb20828165bb211ed937a0242ac120002);
+
+--
+-- Déchargement des données de la table `t_class`
+--
+
+INSERT INTO `t_class` (`idClass`, `name`, `fkPerson`, `fkFormation`) VALUES
+(0xad97273f5bb611ed937a0242ac120002, 'MIN', 0xb2082c745bb211ed937a0242ac120002, 0xed6100165bb511ed937a0242ac120002),
+(0xad972b865bb611ed937a0242ac120002, 'CIN', 0xb2082c745bb211ed937a0242ac120002, 0xed6106065bb511ed937a0242ac120002),
+(0xad972de95bb611ed937a0242ac120002, 'CID', 0xb2082b5f5bb211ed937a0242ac120002, 0xed61070e5bb511ed937a0242ac120002),
+(0xad972eec5bb611ed937a0242ac120002, 'MAM', 0xb2082b5f5bb211ed937a0242ac120002, 0xed6107855bb511ed937a0242ac120002),
+(0xad972f6f5bb611ed937a0242ac120002, 'MID', 0xb2082a4c5bb211ed937a0242ac120002, 0xed6107f05bb511ed937a0242ac120002);
+
+
+
+
+--
+-- Déchargement des données de la table `t_student`
+--
+
+INSERT INTO `t_student` (`fkPerson`, `fkSection`, `fkClass`) VALUES
+(0xb20818d15bb211ed937a0242ac120002, 0xb20077c45bb211ed937a0242ac120002, 0xad97273f5bb611ed937a0242ac120002),
+(0xb2081fcf5bb211ed937a0242ac120002, 0xb20077c45bb211ed937a0242ac120002, 0xad97273f5bb611ed937a0242ac120002),
+(0xb20823425bb211ed937a0242ac120002, 0xb2007bb05bb211ed937a0242ac120002, 0xad972b865bb611ed937a0242ac120002),
+(0xb20825bd5bb211ed937a0242ac120002, 0xb2007bb05bb211ed937a0242ac120002, 0xad972b865bb611ed937a0242ac120002),
+(0xb20826f35bb211ed937a0242ac120002, 0xb2007dc55bb211ed937a0242ac120002, 0xad972de95bb611ed937a0242ac120002);
+
+
+--
+-- Déchargement des données de la table `t_TPI`
+--
+
+INSERT INTO `t_TPI` (`idTPI`, `grade`, `fkPerson`) VALUES
+(0xdfd2cb535bb711ed937a0242ac120002, 5.25, 0xb20818d15bb211ed937a0242ac120002),
+(0xdfd2cf715bb711ed937a0242ac120002, 3.7, 0xb2081fcf5bb211ed937a0242ac120002),
+(0xdfd2d19b5bb711ed937a0242ac120002, 4.75, 0xb20823425bb211ed937a0242ac120002),
+(0xdfd2d2795bb711ed937a0242ac120002, 5.35, 0xb20825bd5bb211ed937a0242ac120002),
+(0xdfd2d2da5bb711ed937a0242ac120002, 3.84, 0xb20826f35bb211ed937a0242ac120002);
+
+
+
+--
+-- Déchargement des données de la table `t_lesson`
+--
+
+INSERT INTO `t_lesson` (`idLesson`, `name`, `fkType`) VALUES
+(0xdfd58dd95bb711ed937a0242ac120002, 'Français', 0xb2055f7e5bb211ed937a0242ac120002),
+(0xdfd592775bb711ed937a0242ac120002, 'Anglais', 0xb2055f7e5bb211ed937a0242ac120002),
+(0xdfd594ec5bb711ed937a0242ac120002, 'Math', 0xb2055f7e5bb211ed937a0242ac120002),
+(0xdfd595cf5bb711ed937a0242ac120002, 'P_mobil', 0xb2055e545bb211ed937a0242ac120002),
+(0xdfd596355bb711ed937a0242ac120002, 'P_proj', 0xb2055e545bb211ed937a0242ac120002);
+
+
+--
+-- Déchargement des données de la table `t_grade`
+--
+
+INSERT INTO `t_grade` (`idGrade`, `weighting`, `grade`, `fkPerson`, `fkLesson`) VALUES
+(0xde73f69e5bb811ed937a0242ac120002, 2, 4.3, 0xb20826f35bb211ed937a0242ac120002, 0xdfd58dd95bb711ed937a0242ac120002),
+(0xde73fac05bb811ed937a0242ac120002, 1, 3.45, 0xb20825bd5bb211ed937a0242ac120002, 0xdfd592775bb711ed937a0242ac120002),
+(0xde73fe0d5bb811ed937a0242ac120002, 1, 5.2, 0xb20823425bb211ed937a0242ac120002, 0xdfd594ec5bb711ed937a0242ac120002),
+(0xde73ff5e5bb811ed937a0242ac120002, 2, 4.81, 0xb2081fcf5bb211ed937a0242ac120002, 0xdfd595cf5bb711ed937a0242ac120002),
+(0xde73fff55bb811ed937a0242ac120002, 1.5, 4.35, 0xb20818d15bb211ed937a0242ac120002, 0xdfd596355bb711ed937a0242ac120002);
+
+
+
+--
+-- Déchargement des données de la table `t_formationPlanLesson`
+--
+
+INSERT INTO `t_formationPlanLesson` (`fkFormation`, `fkLesson`, `beginDate`, `endDate`) VALUES
+(0xed6100165bb511ed937a0242ac120002, 0xdfd58dd95bb711ed937a0242ac120002, '2022-04-20', '2022-08-26'),
+(0xed6100165bb511ed937a0242ac120002, 0xdfd592775bb711ed937a0242ac120002, '2022-06-15', '2022-11-26'),
+(0xed6106065bb511ed937a0242ac120002, 0xdfd592775bb711ed937a0242ac120002, '2022-08-23', '2022-09-15'),
+(0xed6106065bb511ed937a0242ac120002, 0xdfd594ec5bb711ed937a0242ac120002, '2022-07-18', '2022-09-13'),
+(0xed6100165bb511ed937a0242ac120002, 0xdfd596355bb711ed937a0242ac120002, '2022-07-05', '2022-08-18');
+
+
+
+
+
+
+
+
+
+
+
